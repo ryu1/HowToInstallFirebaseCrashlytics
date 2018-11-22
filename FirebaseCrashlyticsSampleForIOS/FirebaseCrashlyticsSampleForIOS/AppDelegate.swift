7f767630d055bd2e7966f563201a900f9c221ad9
Ryuichi Ishitsuka <jeremy.hillary.boob@gmail.com>
2018-11-22 12:20:55 +0900

    Add Firebase And Crashlytics.

--- FirebaseCrashlyticsSampleForIOS/FirebaseCrashlyticsSampleForIOS/AppDelegate.swift
+++ FirebaseCrashlyticsSampleForIOS/FirebaseCrashlyticsSampleForIOS/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Firebase
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
+        Fabric.with([Crashlytics.self])
+        
         return true
     }
 
